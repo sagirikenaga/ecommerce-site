7f8c19f1eeaee78f20c4110b7744375c9ae6b813
Sagiri <sagiri.ikenaga@ryerson.ca>
2022-10-29 17:37:42 -0400

    get routes finished for tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,14 +3,38 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({
+      include: [{model: Product}],
+    });
+    if(!tagData){
+      res.status(400).json({message: 'No tag data.'});
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json({message: `Internal server error: ${err}`});
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{model: Product}],
+    });
+    if (!tagData){
+      res.status(400).json({message: 'No tag data for this id.'});
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json({message: `Interval server error: ${err}`});
+  }
 });
 
 router.post('/', (req, res) => {
