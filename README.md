5ac10cc8a94b9bd7195569dea593d9700a7a53cd
Sagiri <sagiri.ikenaga@ryerson.ca>
2022-11-16 00:52:45 -0500

    updated readme and final changes

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# E-commerce Back End Starter Code
+# Module 13 Challenge: E-commerce Back End
+
+## Description
+
+This project was build so that a retail store may keep track of their inventory in a virtual setting. The user may use this code to access their SQL database and access data on their products, categories, and tags. With the products, the user can keep track of the product name, price, stock, and associated categories and tags regarding the product. This project allowed me to apply my knowledge learned about SQL and sequelize to understand the backend data storage used in some websites. 
+
+## Installation
+
+Clone/fork this repository at https://github.com/sagirikenaga/ecommerce-site. Then run the commands: npm i, then mysource -u root -p and enter in your password. To seed the data, run the command seeds/index.js. Lastly, run the command npm start to initialize. The data can be accessed using Insomnia.
+
+## Usage
+
+This project is used by following the installation process described above, then accessing the data through Insomnia. With insomnia, using the localhost and PORT described in the server.js file, the different routes can be accessed. For example, in order to access the page containing all the category data, the link http://localhost:3001/api/categories/ can be inputted into the html bar and "GET" can be selected. The results will display on the right. In order to add either a category, tag, or product, the user may input the objects using a POST route. The PUT and DELETE routes may also be used in order to update and delete an item, respectively. For more information, please view the video linked below.
+
+## Video Demonstration
+
+Link to the live demonstration video can be found here: https://youtu.be/A5NTuYXhwfM.
+
+## Credits
+
+Sagiri Ikenaga
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,70 +1,108 @@
-const router = require('express').Router();
-const { restart } = require('nodemon');
-const { Product, Category, Tag, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { restart } = require("nodemon");
+const { Product, Category, Tag, ProductTag } = require("../../models");
 
 // The `/api/products` endpoint
 
 // get route for retrieving all products
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const productData = await Product.findAll({
       include: [
-        {model: Category}, 
-        {model: Tag,
-        through: {
-          model: ProductTag,
-          attributes: ['id', 'tag_id','product_id']
+        { model: Category },
+        {
+          model: Tag,
+          through: {
+            model: ProductTag,
+            attributes: ["id", "tag_id", "product_id"],
+          },
         },
-      }],
+      ],
     });
-      if (!productData){
-        res.status(400).json({message: 'No products added.'});
-        return;
-      }
-      res.status(200).json(productData);
-      } catch (err) {
-        res.status(500).json({message: `Internal server error: ${err}`});
-      }
-    });
-
-// get route for retrieving product by its id 
-router.get('/:id', async (req, res) => {
-  try {
-    const productData = await Product.findByPk(req.params.id, {
-      include: [
-        {model: Category},
-        {model: Tag,
-        through: {
-          model: ProductTag,
-          attributes: ['id','tag_id','product_id']
-        },
-      }],
-    });
-    if (!productData){
-      return res.status(400).json({message: 'No product associated with this id.'});
+    if (!productData) {
+      res.status(400).json({ message: "No products added." });
+      return;
     }
     res.status(200).json(productData);
   } catch (err) {
-      res.status(500).json({message: `Interval server error: ${err}`});
-    };
+    res.status(500).json({ message: `Internal server error: ${err}` });
+  }
+});
+
+// get route for retrieving product by its id
+router.get("/:id", async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [
+        { model: Category },
+        {
+          model: Tag,
+          through: {
+            model: ProductTag,
+            attributes: ["id", "tag_id", "product_id"],
+          },
+        },
+      ],
+    });
+    if (!productData) {
+      return res
+        .status(400)
+        .json({ message: "No product associated with this id." });
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json({ message: `Interval server error: ${err}` });
+  }
+});
 
 // create new product
-router.post('/', (req, res) => {
-  Product.create({
-    product_name: req.body.product_name,
-    price: req.body.price,
-    stock: req.body.stock,
-  })
-    .then((newProduct) => {
-      res.json(newProduct);
+// router.post('/', (req, res) => {
+//   Product.create({
+//     product_name: req.body.product_name,
+//     price: req.body.price,
+//     stock: req.body.stock,
+//   })
+//     .then((newProduct) => {
+//       res.json(newProduct);
+//     })
+//     .catch((err) => {
+//       res.status(500).json(err);
+//     })
+// });
+
+router.post("/", (req, res) => {
+  /* req.body should look like this...
+    {
+      product_name: "Basketball",
+      price: 200.00,
+      stock: 3,
+      tagIds: [1, 2, 3, 4]
+    }
+  */
+  Product.create(req.body)
+    .then((product) => {
+      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+      if (req.body.tagIds.length) {
+        const productTagIdArr = req.body.tagIds.map((tag_id) => {
+          return {
+            product_id: product.id,
+            tag_id,
+          };
+        });
+        return ProductTag.bulkCreate(productTagIdArr);
+      }
+      // if no product tags, just respond
+      res.status(200).json(product);
     })
+    .then((productTagIds) => res.status(200).json(productTagIds))
     .catch((err) => {
-      res.status(500).json(err);
-    })
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -103,22 +141,23 @@ router.put('/:id', (req, res) => {
       // console.log(err);
       res.status(400).json(err);
     });
-})
 });
 
 //delete route for deleting product by its id
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   try {
     const productData = await Product.destroy({
-      where: {id: req.params.id}
+      where: { id: req.params.id },
     });
     if (!productData) {
-      return res.status(400).json({message: 'No product associated with this id.'});
+      return res
+        .status(400)
+        .json({ message: "No product associated with this id." });
     }
     res.status(200).json(productData);
   } catch (err) {
-    res.status(500).json({message: `Internal server error: ${err}`});
-  };
+    res.status(500).json({ message: `Internal server error: ${err}` });
+  }
 });
 
 module.exports = router;
